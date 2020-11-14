b04e9b31aee6b9d14246e46342e7fea08384250b
Feyris-Tan <ft@feyris-tan.lebtimnetz.de>
2020-11-14 19:59:03 +0100

    Added "colormatrix" Tool

---
+++ colormatrix/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace colormatrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(624, 441);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "colormatrix";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ colormatrix/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace colormatrix
+{
+    public partial class Form1 : Form
+    {
+        private Color[] vgacolors;
+        private int[] currentMatrix;
+        private Panel[] panels;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            vgacolors = new Color[]
+            {
+                Color.FromArgb(0,0,0),
+                Color.FromArgb(128,0,0),
+                Color.FromArgb(0,128,0),
+                Color.FromArgb(128,128,0),
+                Color.FromArgb(0,0,128),
+                Color.FromArgb(128,0,128),
+                Color.FromArgb(0,128,128),
+                Color.FromArgb(192,192,192),
+                Color.FromArgb(128,128,128),
+                Color.FromArgb(255,0,0),
+                Color.FromArgb(0,255,0),
+                Color.FromArgb(255,255,0),
+                Color.FromArgb(0,0,255),
+                Color.FromArgb(255,0,255),
+                Color.FromArgb(0,255,255),
+                Color.FromArgb(255,255,255)
+            };
+            int total = tableLayoutPanel1.ColumnCount * tableLayoutPanel1.RowCount;
+            currentMatrix = new int[total];
+            panels = new Panel[total];
+
+            for (int y = 0; y < tableLayoutPanel1.RowCount; y++)
+            {
+                for (int x = 0; x < tableLayoutPanel1.ColumnCount; x++)
+                {
+                    Panel panel = new Panel();
+                    panel.Dock = DockStyle.Fill;
+                    panel.Margin = new Padding(0, 0, 0, 0);
+                    tableLayoutPanel1.Controls.Add(panel, x, y);
+                    panels[(y * tableLayoutPanel1.RowCount) + x] = panel;
+                }
+            }
+
+            ApplyColors();
+        }
+
+        private void ApplyColors()
+        {
+            for (int i = 0; i < currentMatrix.Length; i++)
+            {
+                panels[i].BackColor = vgacolors[currentMatrix[i]];
+            }
+        }
+
+        private void Increment()
+        {
+            for (int i = 0; i < currentMatrix.Length; i++)
+            {
+                currentMatrix[i]++;
+                if (currentMatrix[i] > (vgacolors.Length - 1))
+                {
+                    currentMatrix[i] = 0;
+                    currentMatrix[i + 1]++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private long frames;
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            Increment();
+            ApplyColors();
+            Text = String.Format("colormatrix {0}", frames++);
+        }
+    }
+}
