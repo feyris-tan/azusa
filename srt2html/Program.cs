c295dbe4879ca0fd002aa526ac0c91d2aac4ae30
Feyris-Tan <ft@feyris-tan.lebtimnetz.de>
2020-11-14 19:58:29 +0100

    Added "srt2html" Tool

---
+++ srt2html/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace srt2html
+{
+    class Program
+    {
+        private static string GuessLangaugeFromFilename(string name)
+        {
+            if (name.Contains("French"))
+                return "fr";
+
+            return "en";
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: srt2html MyFavoriteMovie.French.srt");
+                return;
+            }
+
+            FileInfo srt = new FileInfo(args[0]);
+            string changedExtension = Path.ChangeExtension(srt.FullName, ".html");
+            FileInfo html = new FileInfo(changedExtension);
+            if (html.Exists)
+                html.Delete();
+
+            StreamReader inLevel1 = srt.OpenText();
+            string firstline = inLevel1.ReadLine();
+            if (!firstline.Equals("1"))
+            {
+                Console.WriteLine("not an srt file!");
+                return;
+            }
+            FileStream outLevel1 = html.OpenWrite();
+            StreamWriter outLevel2 = new StreamWriter(outLevel1, Encoding.UTF8);
+
+            outLevel2.WriteLine("<!DOCTYPE html>");
+            outLevel2.WriteLine("<html lang=\"{0}\">",GuessLangaugeFromFilename(srt.Name));
+            outLevel2.WriteLine("\t<head>");
+            outLevel2.WriteLine("\t\t<title>{0}</title>", srt.Name);
+            outLevel2.WriteLine("\t\t<meta charset=\"UTF-8\">");
+            outLevel2.WriteLine("\t\t<meta name=\"author\" content=\"{0}\">",Environment.UserName);
+            outLevel2.WriteLine("\t\t<meta name=\"robots\" content=\"noindex\"/>");
+            outLevel2.WriteLine("\t\t<meta name=\"generator\" content=\"srt2html\">");
+            outLevel2.WriteLine("\t</head>");
+            outLevel2.WriteLine("\t<body style=\"background-color: #000; color:#fff; \">");
+            outLevel2.WriteLine("\t\t{0}<br/>", firstline);
+            while (!inLevel1.EndOfStream)
+            {
+                string inLine = inLevel1.ReadLine();
+                inLine = HttpUtility.HtmlEncode(inLine);
+                outLevel2.WriteLine("\t\t{0}<br/>", inLine);
+            }
+            outLevel2.WriteLine("\t</body>");
+            outLevel2.WriteLine("</html>");
+            outLevel2.Flush();
+            outLevel2.Close();
+            outLevel1.Close();
+            inLevel1.Close();
+        }
+    }
+}
---
+++ srt2html/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("srt2html")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("srt2html")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("f0a2fa1f-f4bd-4819-9a73-b46a47134176")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
